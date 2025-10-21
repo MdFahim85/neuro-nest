e59a89ec6ad0263b80ffbfab37ee77bb681bab1e
Md.Fahim <mortuza.aziz.47@gmail.com>
2025-10-21 12:41:26 +0600

    User posts and saved posts routes implemented

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import prisma from "../config/prisma";
+import { supabase } from "../config/supabaseClient";
 
 // Get personal details
 export const getMyDetails = async (req: Request, res: Response) => {
@@ -59,6 +60,65 @@ export const updateMyDetails = async (req: Request, res: Response) => {
     return res
       .status(200)
       .json({ message: "Successfully updated user details", updatedUser });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// Update password
+export const changeUserPassword = async (req: Request, res: Response) => {
+  try {
+    const { newPassword } = req.body;
+    const user = req.user;
+    const userId = user?.id as string;
+
+    const { data, error } = await supabase.auth.admin.updateUserById(userId, {
+      password: newPassword,
+    });
+
+    if (error) {
+      return res.status(400).json({ error: error.message });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Password updated successfully", data });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// Get user posts (#pagination required)
+export const getUserPosts = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.userId;
+    const user = await prisma.user.findUnique({ where: { id } });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const posts = await prisma.post.findMany({ where: { authorid: user.id } });
+    if (!posts || !posts.length) {
+      return res.status(404).json({ message: "Posts not found" });
+    }
+    return res.status(200).json({ message: "User posts found", posts });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getSavedPosts = async (req: Request, res: Response) => {
+  try {
+    const user = req.user;
+    const userSavedPosts = await prisma.user.findUnique({
+      where: { id: user?.id },
+      select: { SavedPost: true },
+    });
+    if (!userSavedPosts || !userSavedPosts.SavedPost.length) {
+      return res.status(404).json({ message: "No saved post found" });
+    }
+    return res
+      .status(200)
+      .json({ message: "Saved posts found", userSavedPosts });
   } catch (error) {
     return res.status(500).json({ error: "Internal server error" });
   }
--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -1,21 +1,48 @@
 import express from "express";
 const router = express.Router();
 import {
+  changeUserPassword,
   getMyDetails,
+  getSavedPosts,
   getUserDetails,
+  getUserPosts,
   updateMyDetails,
 } from "../controllers/userController";
 import { authMiddleware, authorizeRoles } from "../middlewares/auth";
 
+// Personal routes
 router
   .route("/me")
-  .get(authMiddleware, authorizeRoles("REGULAR_USER", "STUDENT"), getMyDetails)
+  .get(
+    authMiddleware,
+    authorizeRoles("REGULAR_USER", "STUDENT", "MODERATOR"),
+    getMyDetails
+  )
   .put(
     authMiddleware,
-    authorizeRoles("REGULAR_USER", "STUDENT"),
+    authorizeRoles("REGULAR_USER", "STUDENT", "MODERATOR"),
     updateMyDetails
   );
 
+router
+  .route("/me/change-password")
+  .put(
+    authMiddleware,
+    authorizeRoles("REGULAR_USER", "STUDENT", "MODERATOR"),
+    changeUserPassword
+  );
+
+router
+  .route("/me/saved-posts")
+  .get(
+    authMiddleware,
+    authorizeRoles("REGULAR_USER", "STUDENT", "MODERATOR"),
+    getSavedPosts
+  );
+
+// User routes
 router.route("/:userId").get(getUserDetails);
+
+router.route("/:userId/posts").get(getUserPosts);
 
 module.exports = router;
